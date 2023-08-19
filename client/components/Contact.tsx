84499b7ea6fb016b6ba7c326c95fce86da7606cd
Simon Yoo <yoosghn0@gmail.com>
2023-08-19 16:15:28 +1200

    added contact page

--- client/components/Contact.tsx
+++ client/components/Contact.tsx
@@ -1,6 +1,25 @@
 function Contact(){
     return(
-        <h1>Return</h1>
+        <div className="contact">
+            <div className="contact-header">
+            <h1>Contact Me</h1>
+            <p>Please contact me directly at yoosghn0@gmail.com ro through this form.</p>
+            </div>
+            <div className="form-container">
+                <form className="form">
+                    <label htmlFor="name"></label>
+                    <input className="form-name"type="text" name="name" id="name" placeholder="Your name" />
+                    <label htmlFor="email"></label>
+                    <input className="form-email"type="text" name="email" id="email" placeholder="Your email"/>
+                    <label htmlFor="msg"></label>
+                    <textarea className="form-msg" name="msg" id="msg" placeholder="Your message" />
+                    <div className="button">
+                    <button className="button-send">Send</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
     )
 }
 
